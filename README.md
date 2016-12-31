4bb8b07147dfdbdff326df391edfd4c62c690f42
Todd <rtoddmorse@gmail.com>
2016-12-31 01:01:33 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Personal Website
+
+A personal website to showcase experience and projects
+
+## About
+
+This is a simple website with no backend. Views are made using pug. Stylesheets are made using SCSS. Any scripts will be writen in es6. Gulp is used to transpile to html, css, and es5 respectively. Express is used to build a static server for testing. The public directory can be used with any static http server to host the website.
+
+## Installing Dependencies
+
+`npm install`
+
+## Building
+
+To build with watch:
+
+`gulp`
+
+To build without watch:
+
+`gulp build`
+
+
+## Usage
+
+To build, start test server, and watch:
+
+`npm start`
+
+To start test server:
+
+`node server.js`
---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var babel = require('gulp-babel');
+var pug = require('gulp-pug');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var fs = require('fs');
+var path = require('path');
+
+gulp.task('default', ['sass', 'scripts', 'views', 'sass:watch', 'scripts:watch', 'views:watch']);
+
+gulp.task('build', ['sass', 'scripts', 'views']);
+
+gulp.task('watch', ['sass:watch', 'scripts:watch', 'views:watch']);
+
+
+gulp.task('sass', function () {
+  return gulp.src(['./sass/globals/*.scss', './sass/pages/*.scss'])
+    .pipe(concat('bundle.min.css'))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('./public/stylesheets/'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('./src/**/*.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(concat('app.min.js'))
+    .pipe(uglify({
+      mangle: false
+    }))
+    .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('views', function buildHTML() {
+  var locals = {};
+  fs.readdirSync("data").forEach(function(filename) {
+    locals[/[a-zA-Z0-9]*/g.exec(filename)] = JSON.parse(fs.readFileSync(path.join('data', filename)))
+  })
+  return gulp.src('views/**/*.pug')
+  .pipe(pug({
+    locals:locals
+  }))
+  .pipe(gulp.dest('./public'))
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('scripts:watch', function () {
+  gulp.watch('./src/**/*.js', ['scripts']);
+});
+
+gulp.task('views:watch', function () {
+  gulp.watch('./views/**/*.pug', ['views']);
+});
